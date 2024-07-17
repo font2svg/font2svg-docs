ec832055b420abcef840bec2ed09152fd2371ee3
Bean <dengbin@bilibili.com>
2024-07-17 11:43:18 +0800

    chore: pnpm version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/eslint'],
+  modules: ['@nuxt/content', '@nuxt/eslint', "@nuxt/image"],
 
   routeRules: {
     '/': { prerender: true },
